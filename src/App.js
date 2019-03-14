a5aad2d566c483323814de311b0ca1bffbcef78b
Siddhartha Nanda <sidnanda@publicisgroupe.net>
2019-03-14 17:55:10 +0530

    fixing bid price

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
     );
 
     newTeams[selectedTeam] = newTeams[selectedTeam] - price;
-    if (( newTeams[selectedTeam] > price && price !== 0 && selectedPlayer.basePrice < price) || !isSold) {
+    if (( newTeams[selectedTeam] > price && price !== 0 && selectedPlayer.basePrice <= price) || !isSold) {
       newPlayerList[index].team = isSold
       ? selectedTeam
       : config.TEAM.UNSOLD_PLAYERS;
